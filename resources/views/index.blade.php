2cee1e709dcb11416e0e92304f016046df7e5eea
jiangxianli <997204035@qq.com>
2019-11-11 16:22:14 +0800

    SEO

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -53,6 +53,21 @@
             s.parentNode.insertBefore(hm, s);
         })();
     </script>
+    <!-- 自动收录推送 -->
+    <script>
+        (function () {
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https') {
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else {
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
+    </script>
 </head>
 <body class="layui-layout-body">
 <div class="layui-layout layui-layout-admin">

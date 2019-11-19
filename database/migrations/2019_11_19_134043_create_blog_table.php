08949f51355953878671b38e579e060815ba6faf
jiangxianli <997204035@qq.com>
2019-11-19 15:36:53 +0800

    每小时热门IP 生成文章

---
+++ database/migrations/2019_11_19_134043_create_blog_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char("date_time",10)->default("")->comment("发布时间");
+            $table->text("content");
+            $table->timestamps();
+
+            $table->unique("date_time");
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}

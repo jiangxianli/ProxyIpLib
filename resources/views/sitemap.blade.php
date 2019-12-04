af390b0066d5469d0b290d1a57b44591e06f5280
jiangxianli <997204035@qq.com>
2019-12-04 09:09:09 +0800

    更新sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,12 +2,14 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ route('web.index') }}</loc>
+        <lastmod>{{ \Carbon\Carbon::now()->format("Y-m-d") }}</lastmod>
         <changefreq>always</changefreq>
         <priority>1.0</priority>
     </url>
     @foreach($countries as $item)
         <url>
             <loc>{{ route('web.index',['country' => $item]) }}</loc>
+            <lastmod>{{ \Carbon\Carbon::now()->format("Y-m-d") }}</lastmod>
             <changefreq>hourly</changefreq>
             <priority>0.9</priority>
         </url>
@@ -15,21 +17,23 @@
     @foreach($isp as $item)
     <url>
         <loc>{{ route('web.index',['isp' => $item]) }}</loc>
+        <lastmod>{{ \Carbon\Carbon::now()->format("Y-m-d") }}</lastmod>
         <changefreq>hourly</changefreq>
         <priority>0.9</priority>
     </url>
     @endforeach
     <url>
         <loc>{{ route('blog.index') }}</loc>
+        <lastmod>{{ \Carbon\Carbon::now()->format("Y-m-d") }}</lastmod>
         <changefreq>hourly</changefreq>
         <priority>0.9</priority>
     </url>
     @foreach ($blogs as $blog)
         <url>
             <loc>{{ route('blog.detail',['blog_id'=>$blog->id]) }}</loc>
-            <lastmod>{{ $blog->updated_at->tz('PRC')->toAtomString() }}</lastmod>
+            <lastmod>{{ $blog->updated_at->format("Y-m-d") }}</lastmod>
             <changefreq>daily</changefreq>
-            <priority>0.9</priority>
+            <priority>0.8</priority>
         </url>
     @endforeach
 </urlset>

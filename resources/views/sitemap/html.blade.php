5cb912d6a3dd111585ab209b6312f20a7ad563a4
jiangxianli <997204035@qq.com>
2019-12-31 17:25:34 +0800

    站点地图

--- resources/views/sitemap/html.blade.php
+++ resources/views/sitemap/html.blade.php
@@ -7,6 +7,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link href="https://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 
+    @include('layout.common_js')
 </head>
 <body>
 

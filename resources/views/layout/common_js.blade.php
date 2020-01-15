7b3bd836277aab2f9d28a75586559200a77bc1db
jiangxianli <997204035@qq.com>
2020-01-15 09:06:18 +0800

    样式调整

--- resources/views/layout/common_js.blade.php
+++ resources/views/layout/common_js.blade.php
@@ -23,10 +23,3 @@
         s.parentNode.insertBefore(bp, s);
     })();
 </script>
-<!-- 360 自动收录推送 -->
-<script>
-    (function () {
-        var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-        document.write('<script src="' + src + '" id="sozz"><\/script>');
-    })();
-</script>
--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,3 +4,10 @@
         <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278566119'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278566119%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));</script>
     </div>
 </div>
+<!-- 360 自动收录推送 -->
+<script>
+    (function () {
+        var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+        document.write('<script src="' + src + '" id="sozz"><\/script>');
+    })();
+</script>

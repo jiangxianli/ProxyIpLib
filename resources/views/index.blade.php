22510030a5983003fd4d6643ffe4ae92b83a180b
jiangxianli <997204035@qq.com>
2020-01-15 10:42:22 +0800

    优化

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -85,10 +85,10 @@
                         {{--<script id="w2898_10507">(function () {var zy = document.createElement("script");var flowExchange = window.location.protocol.split(":")[0];var http = flowExchange === "https"?"https":"http";zy.src = http+"://exchange.2898.com/index/flowexchange/getGoods?id=10507&sign=c9a7c5527bca21f6ea3654c48b2fcb11";var s = document.getElementsByTagName("script");for(var i=0;i< s.length;i++){if(s[i].id){if(s[i].id == "w2898_10507"){s[i].parentNode.insertBefore(zy, s[i]);continue;}}}})();</script>--}}
                     {{--</div>--}}
                     {{--<div id="ad-1-image" class="ad-card"></div>--}}
-                    {{--<p  >本项目已坚持无偿运行2年，您也可以直接小额支付请我喝杯咖啡，鼓励下呗~</p>--}}
-                    {{--<hr class="layui-bg-cyan">--}}
-                    {{--<div class="ad-card"><img src="https://cdn.shortpixel.ai/client/q_glossy,ret_img/https://www.jiangxianli.com/wp-content/uploads/2018/06/QQ%E6%88%AA%E5%9B%BE20180628153840.png"></div>--}}
-                    {{--<div  class="ad-card"><img src="https://cdn.shortpixel.ai/client/q_glossy,ret_img/https://www.jiangxianli.com/wp-content/uploads/2018/06/QQ%E6%88%AA%E5%9B%BE20180628153858.png"></div>--}}
+                    <p  >本项目已坚持无偿运行2年，您也可以直接小额支付支持鼓励下呗~</p>
+                    <hr class="layui-bg-cyan">
+                    <div class="ad-card"><img src="/wechat-pay.png"></div>
+                    <div  class="ad-card"><img src="/ali-pay.png"></div>
                 </div>
             </div>
         </div>

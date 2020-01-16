15763eda42d2740082762a7c345279d2c4657dfe
jiangxianli <997204035@qq.com>
2020-01-16 09:19:07 +0800

    SEO 优化

--- resources/views/blog/detail.blade.php
+++ resources/views/blog/detail.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta name="keywords" content="免费代理IP,代理IP,高匿IP,优质IP,全球免费代理"/>
-    <meta name="description" content="全球免费代理IP库，高可用IP，精心筛选优质IP,5s必达"/>
+    <meta name="keywords" content="免费代理IP,代理IP,高匿IP,优质IP,全球免费代理,最新IP"/>
+    <meta name="description" content="全球免费代理IP库，高可用IP，精心筛选优质IP，2s必达,每秒持续更新"/>
     <title>{{ sprintf("%s 全球最新免费HTTP代理IP",date("Y年m月d日H时",strtotime($blog['created_at']))) }}- 高可用全球免费代理IP库</title>
     <link rel="stylesheet" href="/layui/css/layui.css">
     <link rel="stylesheet" href="/css/main.css">

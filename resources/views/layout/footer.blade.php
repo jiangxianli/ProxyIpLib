d736c3f973d2a1f6afcbe8f6a6b111a3aa91f39a
jiangxianli <997204035@qq.com>
2020-03-17 14:34:55 +0800

    update

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="content-footer">
     <div class="footer-cnzz">
         <em>Copyright © 2019 高可用全球免费代理IP库</em>
-        <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278566119'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278566119%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));</script>
+        <script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1278691459'%3E%3C/span%3E%3Cscript src='https://v1.cnzz.com/z_stat.php%3Fid%3D1278691459%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));</script>
         <span>&nbsp;&nbsp;<a href="{{ route('sitemap.html') }}">站点地图</a> </span>
     </div>
 </div>

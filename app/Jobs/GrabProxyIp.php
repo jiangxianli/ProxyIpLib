8b3059ab57e60dbad3ff6e187659017767a1b553
jiangxianli <997204035@qq.com>
2020-04-13 14:34:04 +0800

    update

--- app/Jobs/GrabProxyIp.php
+++ app/Jobs/GrabProxyIp.php
@@ -49,55 +49,64 @@ class GrabProxyIp extends Job
      */
     public function handle(ProxyIpBusiness $proxy_ip_business)
     {
+        try {
 
-        //实例化Redis
-        $redis = app('redis');
+            //实例化Redis
+            $redis = app('redis');
 
-        $grab_key = "proxy_ips:grab_flag:" . $this->origin;
-        //正在抓取中
-        if ($redis->exists($grab_key)) {
-            return;
+            $grab_key = "proxy_ips:grab_flag:" . $this->origin;
+            //正在抓取中
+            if ($redis->exists($grab_key)) {
+                return;
+            }
+
+            $redis->set($grab_key, time());
+            $redis->expireAt($grab_key, time() + 300);
+
+            switch ($this->origin) {
+                case 'kuidaili':
+                    $proxy_ip_business->grabKuaiDaiLi();
+                    return;
+                case 'ip3366':
+                    $proxy_ip_business->grabIp3366();
+                    return;
+                case '89ip':
+                    $proxy_ip_business->grab89Ip();
+                    return;
+                case 'xila':
+                    $proxy_ip_business->xiLaIp();
+                    return;
+                case 'emailtry':
+                    $proxy_ip_business->emailtryIp();
+                    return;
+                case 'qinghua':
+                    $proxy_ip_business->qinghuaIp();
+                    return;
+                case 'xsdaili':
+                    $proxy_ip_business->xsdailiIp();
+                    return;
+                case 'kxdaili':
+                    $proxy_ip_business->kxdailiIp();
+                    return;
+                case 'nima':
+                    $proxy_ip_business->nimaIp();
+                    return;
+                case 'super':
+                    $proxy_ip_business->superIp();
+                    return;
+                case 'xici':
+                    $proxy_ip_business->xiciIp();
+                    return;
+            }
+
+            $this->delete();
+
+        } catch (\Exception $exception) {
+            app("Logger")->error("抓取网页错误", [
+                'err_code'  => $exception->getCode(),
+                'err_msg'   => $exception->getMessage(),
+                'err_trace' => $exception->getTraceAsString()
+            ]);
         }
-
-        $redis->set($grab_key, time());
-        $redis->expireAt($grab_key, time() + 300);
-
-        switch ($this->origin) {
-            case 'kuidaili':
-                $proxy_ip_business->grabKuaiDaiLi();
-                return;
-            case 'ip3366':
-                $proxy_ip_business->grabIp3366();
-                return;
-            case '89ip':
-                $proxy_ip_business->grab89Ip();
-                return;
-            case 'xila':
-                $proxy_ip_business->xiLaIp();
-                return;
-            case 'emailtry':
-                $proxy_ip_business->emailtryIp();
-                return;
-            case 'qinghua':
-                $proxy_ip_business->qinghuaIp();
-                return;
-            case 'xsdaili':
-                $proxy_ip_business->xsdailiIp();
-                return;
-            case 'kxdaili':
-                $proxy_ip_business->kxdailiIp();
-                return;
-            case 'nima':
-                $proxy_ip_business->nimaIp();
-                return;
-            case 'super':
-                $proxy_ip_business->superIp();
-                return;
-            case 'xici':
-                $proxy_ip_business->xiciIp();
-                return;
-        }
-
-        $this->delete();
     }
 }

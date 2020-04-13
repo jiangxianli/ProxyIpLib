a9ac74d1721341bc3f56161d80d4741dfe69df5e
jiangxianli <997204035@qq.com>
2020-04-13 16:15:58 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -92,11 +92,11 @@
                     {{--<script id="w2898_10507">(function () {var zy = document.createElement("script");var flowExchange = window.location.protocol.split(":")[0];var http = flowExchange === "https"?"https":"http";zy.src = http+"://exchange.2898.com/index/flowexchange/getGoods?id=10507&sign=c9a7c5527bca21f6ea3654c48b2fcb11";var s = document.getElementsByTagName("script");for(var i=0;i< s.length;i++){if(s[i].id){if(s[i].id == "w2898_10507"){s[i].parentNode.insertBefore(zy, s[i]);continue;}}}})();</script>--}}
                     {{--</div>--}}
                     {{--<div id="ad-1-image" class="ad-card"></div>--}}
-                    <p>本项目已坚持无偿运行2年，您也可以直接小额支付支持鼓励下呗~</p>
+                    <p>帮忙关注下呗，京东优惠券省钱小帮手，来领券哦~</p>
                     <hr class="layui-bg-cyan">
                     <div class="ad-card">
-                        <img src="/wechat-pay.png" class="ad-img">
-                        <img src="/ali-pay.png" class="ad-img">
+                        <img src="{{ asset("/images/qrcode.png") }}" >
+                        {{--<img src="/ali-pay.png" class="ad-img">--}}
                     </div>
                 </div>
             </div>

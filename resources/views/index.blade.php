2a19dfd8d1b14f7d1a22ceb927c8e4e000afb822
jiangxianli <997204035@qq.com>
2020-11-26 11:24:23 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -92,7 +92,7 @@
                     {{--<script id="w2898_10507">(function () {var zy = document.createElement("script");var flowExchange = window.location.protocol.split(":")[0];var http = flowExchange === "https"?"https":"http";zy.src = http+"://exchange.2898.com/index/flowexchange/getGoods?id=10507&sign=c9a7c5527bca21f6ea3654c48b2fcb11";var s = document.getElementsByTagName("script");for(var i=0;i< s.length;i++){if(s[i].id){if(s[i].id == "w2898_10507"){s[i].parentNode.insertBefore(zy, s[i]);continue;}}}})();</script>--}}
                     {{--</div>--}}
                     {{--<div id="ad-1-image" class="ad-card"></div>--}}
-                    <p style="color:red">帮忙关注下呗，京东优惠券省钱小帮手，来领券哦~</p>
+                    <p style="color:red">帮忙关注下公众号呗，淘宝、京东商品转链下单均可高额返利！~</p>
                     <hr class="layui-bg-cyan">
                     <div class="ad-card">
                         <img src="{{ asset("/images/qrcode.png") }}" >
